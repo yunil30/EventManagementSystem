b908097d800fc71d3085610aeacd5cdc9dd1d381
yunil30 <yunildungo@gmail.com>
2025-07-07 17:38:12 +0800

    07-07-2025

--- resources/views/authentication/LoginUser.blade.php
+++ resources/views/authentication/LoginUser.blade.php
@@ -6,9 +6,11 @@
     <title>Event Manager</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free/css/all.min.css">
 </head>
+
 <style>
-    body {
+    body {  
         background-color: #f6f6f6;
         position: relative;
         height: 100vh;
@@ -56,6 +58,109 @@
                 font-weight: 600;
                 letter-spacing: .5px;
             }
+
+            #btnLogin {
+                background-color: #212529;
+                color: #ffffff;
+                font-size: 14px;
+                letter-spacing: .5px;
+                border-radius: 0px;
+                border: 1px solid #dee2e6;
+                width: 100%;
+                padding: 10px 40px;
+                margin: 0px;
+
+                &:hover {
+                    background-color: #343a40;
+                    color: #fff;
+                    cursor: pointer;
+                }
+
+                &:active {
+                    background-color: #212529;
+                    transform: translateY(2px);
+                }
+
+                &:disabled {
+                    background-color: #8c8f92;
+                    cursor: not-allowed;
+                    border: 1px solid #dee2e6;
+                }
+            }
+
+            .form-group {
+                margin-bottom: 16px;
+                padding: 0px;
+                position: relative;
+
+                .input-icon {
+                    position: absolute;
+                    top: 59%;
+                    left: 13px;
+                    font-size: 16px;
+                    color: #8c8f92;
+                }
+
+                label {
+                    font-size: 14px;
+                    letter-spacing: .5px;
+                    text-shadow: 0px 0px 1px rgba(23, 32, 42, 0.8);
+                    margin-bottom: 5px;
+                }
+
+                a {
+                    text-decoration-line: none;
+                    font-size: 14px;
+                    letter-spacing: .5px;
+                    text-shadow: 0px 0px 1px rgba(23, 32, 42, 0.8);
+                    margin-bottom: 5px;
+
+                    &:hover {
+                        text-decoration-line: underline;
+                        font-size: 14px;
+                        letter-spacing: .5px;
+                        text-shadow: 0px 0px 1px rgba(23, 32, 42, 0.8);
+                        margin-bottom: 5px;
+                    }
+                }
+
+                input {
+                    color: #1f2328;
+                    font-size: 14px;
+                    letter-spacing: .5px;
+                    border-radius: 0px;
+                    border: 1px solid #dee2e6;
+                    padding: 10px 40px;
+                    box-shadow: 0px 0px 1px rgba(23, 32, 42, 0.3);
+
+                    &::placeholder {
+                        font-size: 14px;
+                        letter-spacing: .5px;
+                        text-shadow: 0px 0px 1px rgba(23, 32, 42, 0.8);
+                        color: #8c8f92;
+                    }
+
+                    &:focus {
+                        border-color: #212529;
+                        font-size: 14px;
+                        letter-spacing: .5px;
+                        color: #1f2328;
+                        outline: none;
+                    }
+                }
+
+                .toggle-password {
+                    position: absolute;
+                    right: 10px;
+                    top: 50%;
+                    transform: translateY(15%);
+                    background: none;
+                    border: none;
+                    cursor: pointer;
+                    font-size: 14px;
+                    color: #8c8f92;
+                }
+            }
         }
     }
 </style>
@@ -70,18 +175,23 @@
         <div class="form-body">
             <h5>Login</h5>
 
-            <div class="col-md-12">
-                <label for="user_name">Username</label><br>
-                <input type="text" name="user_name" id="user_name" required>
+            <div class="form-group">
+                <i class="fa-solid fa-envelope input-icon"></i>
+                <label for="user_name">Username</label>
+                <input type="text" class="form-control" id="user_name" name="user_name" placeholder="Username" required>
+            </div>
+         
+            <div class="form-group">
+                <i class="fa-solid fa-lock input-icon"></i>
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
+                <button type="button" class="toggle-password" onclick="togglePassword()">
+                    <i class="fa-solid fa-eye"></i>
+                </button>
             </div>
 
-            <div class="col-md-12">
-                <label for="password">Password</label><br>
-                <input type="password" name="password" id="password" required>
-            </div>
-
-            <div class="col-md-12">
-                <button type="submit">Login</button>
+            <div class="form-group">
+                <button type="submit" class="btnLogin" id="btnLogin">Login</button>
             </div>
         </div>
 
@@ -97,3 +207,20 @@
     </form>
 </body>
 </html>
+
+<script>
+    function togglePassword() {
+        const passwordField = document.getElementById("password");
+        const toggleButton = document.querySelector(".toggle-password i");
+
+        if (passwordField.type === "password") {
+            passwordField.type = "text";
+            toggleButton.classList.remove("fa-eye");
+            toggleButton.classList.add("fa-eye-slash");
+        } else {
+            passwordField.type = "password";
+            toggleButton.classList.remove("fa-eye-slash");
+            toggleButton.classList.add("fa-eye");
+        }
+    }
+</script>
